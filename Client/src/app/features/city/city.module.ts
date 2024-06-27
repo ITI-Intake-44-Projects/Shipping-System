206704f34d71d6cdd03b3064410aa3255f3f4dc3
YouSeF <yfrouk853@gmail.com>
2024-06-27 16:30:25 +0300

    adding city component

--- Client/src/app/features/city/city.module.ts
+++ Client/src/app/features/city/city.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { CityRoutingModule } from './city-routing.module';
 import { CityTableComponent } from './city-table/city-table.component';
+import { CityComponent } from './city/city.component';
+import { GovernateModule } from '../governate/governate.module';
+import { SharedModule } from '../../shared/modules/shared/shared.module';
 
 
 @NgModule({
   declarations: [
-    CityTableComponent
+    CityTableComponent,
+    CityComponent,
+
   ],
   imports: [
     CommonModule,
-    CityRoutingModule
+    CityRoutingModule,
+    SharedModule
   ]
 })
 export class CityModule { }
--- Client/src/app/shared/modules/shared/shared.module.ts
+++ Client/src/app/shared/modules/shared/shared.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { PaginationComponent } from '../../components/pagination/pagination.component';
 
 
 
 @NgModule({
-  declarations: [NavbarComponent, FooterComponent],
+  declarations: [NavbarComponent, FooterComponent,PaginationComponent],
   imports: [CommonModule],
-  exports: [NavbarComponent, FooterComponent]
+  exports: [NavbarComponent, FooterComponent,PaginationComponent]
 })
 export class SharedModule { }
