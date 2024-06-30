a13813f1746fd9103b781396391059b7eda4e2b7
Yousef-Farouk <yfrouk853@gmail.com>
2024-06-30 21:28:23 +0300

    fixing group_privelege and other entities

--- WebApi/ShippingSystem/ShippingSystem/Migrations/20240630182701_yousef-v3.Designer.cs
+++ WebApi/ShippingSystem/ShippingSystem/Migrations/20240630182701_yousef-v3.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ShippingSystem.Models;
 
@@ -11,9 +12,11 @@ using ShippingSystem.Models;
 namespace ShippingSystem.Migrations
 {
     [DbContext(typeof(ShippingContext))]
-    partial class ShippingContextModelSnapshot : ModelSnapshot
+    [Migration("20240630182701_yousef-v3")]
+    partial class yousefv3
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
