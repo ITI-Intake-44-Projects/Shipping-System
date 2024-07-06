55ee057288c729607ec0af5453318a0e926f7c21
MSB <khokhakammar@gmail.com>
2024-07-06 14:25:07 +0300

    frontend done without navbar

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { environment } from '../environments/environment';
+import { HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from './shared/modules/shared/shared.module';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+// import { AuthModule } from './features/auth/auth.module';
+// import { MerchantsModule } from './features/merchant/merchants.module';
+// import { ShowOrdersComponent } from './features/show-orders/show-orders.component';
+// import { MatFormFieldModule } from '@angular/material/form-field';
+// import { MatInputModule } from '@angular/material/input';
+// import { MatIconModule } from '@angular/material/icon';
+// import { MatButtonModule } from '@angular/material/button';
+// import { MatTableModule } from '@angular/material/table';
+// import { MatPaginatorModule } from '@angular/material/paginator';
+
+@NgModule({
+  declarations: [AppComponent, SidebarComponent,NavbarComponent],
+  imports: [
+    BrowserModule, AppRoutingModule, HttpClientModule,
+    FormsModule, ReactiveFormsModule, SharedModule,
+  ],
+  providers: [
+    provideClientHydration(), provideHttpClient(withFetch()),
+    { provide: 'apiUrl', useValue: environment.apiUrl },
+    provideAnimationsAsync()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
