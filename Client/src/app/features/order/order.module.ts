1c1b4304aca15b94a9f9344c44946d9af211d010
Yousef-Farouk <yfrouk853@gmail.com>
2024-07-07 04:49:38 +0300

    fixing order reports

--- Client/src/app/features/order/order.module.ts
+++ Client/src/app/features/order/order.module.ts
@@ -4,17 +4,31 @@ import { OrderRoutingModule } from './order-routing.module';
 import { OrderFormComponent } from './order-form/order-form.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AllOrdersComponent } from './all-orders/all-orders.component';
+import { OrderReportsComponent } from './order-reports/order-reports.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [
     OrderFormComponent,
-    AllOrdersComponent
+    AllOrdersComponent,
+    OrderReportsComponent
   ],
   imports: [
     CommonModule,
     OrderRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatButtonModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ]
 })
 export class OrderModule { }
